3dcead352b593a64ab1a5ad9b4743a0466eb3aae
NuttyLee22 <giovanna.nuccim@gmail.com>
2020-07-12 19:42:45 -0300

    tirei o IF chato la

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjetoHospital
-Um projeto para a disciplina de Eletiva Script do curso de ADS da FATEC Mogi das Cruzes
--- conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	$conexao=mysqli_connect("localhost","root","","bd_hospital") or die ("Falha de conexao".mysqli_error());
-
-?>
